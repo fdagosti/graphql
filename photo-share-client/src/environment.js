b9126dc1de0a7ccbf49a62c615092878e698301c
Francois D'Agostini <francois.dagostini@gmail.com>
2019-01-20 22:26:15 +0100

    use docker-compose to setup the project

--- photo-share-client/src/environment.js
+++ photo-share-client/src/environment.js
@@ -1,11 +1,11 @@
-export const environment = {
+const prod_environment = {
     production: true,
     backendUrl: "https://photo-graphql.herokuapp.com",
     backendAPIUrl: "https://photo-graphql.herokuapp.com/graphql",
     backendSubsUrl: "ws://photo-graphql.herokuapp.com/graphql"
 }
 
-const dev_environment = {
+export const environment = {
     production: false,
     backendUrl: "http://localhost:4000",
     backendAPIUrl: "http://localhost:4000/graphql",
